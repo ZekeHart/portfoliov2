0a2fbac9992a80a80abcfb0a2ae1d2eea67740da
Zeke Hart <heirloch@gmail.com>
2019-08-11 18:46:09 -0400

    intial commit with very very basic vue portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+  <div id="app">    
+    <img alt="Zeke head shot" class="headShot" src="./assets/HeadShotEdited.jpeg">    
+    <component1 msg="Zeke's Portfolio"/>  
   </div>
 </template>
+ 
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
+import component1 from './components/component1.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    component1
   }
 }
 </script>
@@ -24,5 +27,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+    background: linear-gradient(45deg, #4a026b, #0d26b3, #1a84db, #0d26b3, #4a026b);
+    background-size: 300% 200%;
+    animation: AnimationName 10s ease infinite;
+}
+
+@keyframes AnimationName { 
+    0%{background-position:0% 50%}
+    50%{background-position:100% 50%}
+    100%{background-position:0% 50%}
+}
+
+.headShot {
+  border-radius: 10%;
+  max-width: 300px;
 }
 </style>
---
+++ src/components/component1.vue
@@ -0,0 +1,17 @@
+<style scoped>
+.demo {  background-color: rgb(96, 230, 230);
+font-size: 3rem;}
+</style>
+
+
+<script>
+export default {  name: 'component1',  
+props: {    msg: String  }}
+</script>
+
+
+<template>  
+    <div class="demo">    
+        <h1>{{ msg }}</h1>  
+    </div>
+</template>
